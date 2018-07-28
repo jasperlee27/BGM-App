3143ec56882ddd751460174b609abd69e77d8b66
Jasper Lee <jasper.lee.2009@vjc.sg>
2018-07-29 01:26:03 +0800

    live bidding static ui

--- src/pages/bidding/bidding.ts
+++ src/pages/bidding/bidding.ts
@@ -14,6 +14,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'bidding.html',
 })
 export class BiddingPage {
+  gameNo: string;
+
   bidName1: string; 
   bidValue1: number;
 
@@ -28,11 +30,17 @@ export class BiddingPage {
 
   bidName5: string; 
   bidValue5: number; 
+  walletBallance: number;
+  betAmount: number;
+  noPlayers: number;
+  currentPoolNumber: number;
+  currentPoolString: string;
 
-
-  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     //can init with call post service leader board
+    this.gameNo= '   12337';
+    this.walletBallance= 1000;
+    this.currentPoolNumber = 2500000;
     this.bidName1 = 'Jasper';
     this.bidValue1 = 4510;
     this.bidName2 = 'KerkWY';
@@ -41,13 +49,33 @@ export class BiddingPage {
     this.bidValue3 = 4427;
     this.bidName4 = 'John';
     this.bidValue4 = 4322;
-    this.bidName5 = 'Jom';
+    this.bidName5 = 'Tom';
     this.bidValue5 = 4239;
 
+    this.currentPoolString = '2,500,000';
+    this.noPlayers=100;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BiddingPage');
   }
 
+  bidGame(){
+
+    this.updateWalletBallance(-this.betAmount);
+    this.updatePoolAmount(+this.betAmount);
+    this.updateNoPlayers();
+  }
+
+  updateNoPlayers(){
+    this.noPlayers += 1;
+  }
+
+  updateWalletBallance(amount: number){
+    this.walletBallance += amount;
+  }
+
+  updatePoolAmount(amount: number){
+    this.currentPoolNumber += amount;
+  }
 }
