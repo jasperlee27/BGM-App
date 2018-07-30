019d9261fe3dd952a5a8b0f68ea6d52ae61898bf
Jasper Lee <jasper.lee.2009@vjc.sg>
2018-07-30 23:11:02 +0800

    interactive treasure hunt refresh

--- src/pages/trehunt/trehunt.ts
+++ src/pages/trehunt/trehunt.ts
@@ -20,12 +20,17 @@ export class TrehuntPage {
   totalETHtix;
   loadBTCProgress;
   loadETHProgress;
+  currBTCprice;
+  currETHprice;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.currBTCtix=7382;
+    this.currBTCprice= this.randomIntRange(8000,10000);
+    this.currETHprice= this.randomIntRange(600,800);
     this.totalBTCtix=8800;
-    this.currETHtix=247;
-    this.totalETHtix=600;
+    this.currBTCtix=this.randomIntRange(1,this.totalBTCtix-1);
+    this.totalETHtix=660;
+    this.currETHtix=this.randomIntRange(1,this.totalETHtix-1);
+
     this.loadBTCProgress= ((this.currBTCtix/this.totalBTCtix)*100).toFixed(2);
     this.loadETHProgress= ((this.currETHtix/this.totalETHtix)*100).toFixed(2);
   }
@@ -34,4 +39,68 @@ export class TrehuntPage {
     console.log('ionViewDidLoad TrehuntPage');
   }
 
+  ngOnInit(){
+
+  }
+
+  doRefresh(refresher) {
+    console.log('Refreshed successfully here', refresher);
+    //to update curr BTC/ETH price & curr BTC/ETH tickets sold
+    var rangeBTCTixIncrease = this.totalBTCtix - this.currBTCtix;
+    var rangeETHTixIncrease = this.totalETHtix - this.currETHtix;
+
+    this.updateBTCETHPrice();
+    this.updateCurrBTCtix(rangeBTCTixIncrease);
+    this.updateCurrETHtix(rangeETHTixIncrease);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+  
+  updateBTCETHPrice(){
+    this.currBTCprice= this.randomIntRange(8000,10000);
+    this.currETHprice= this.randomIntRange(600,800);
+  }
+
+  updateCurrBTCtix(rangeBTCTixIncrease: number){
+    var increment = this.randomIntRange(0,500);
+    // console.log("increment value " + increment);
+    var targetValue = this.currBTCtix + increment;
+    
+    if (targetValue >= this.totalBTCtix){
+      targetValue=this.totalBTCtix;
+    }
+
+    let interval = setInterval(()=>{
+      this.currBTCtix ++;
+      this.loadBTCProgress= ((this.currBTCtix/this.totalBTCtix)*100).toFixed(2);
+      console.log("BTC tix target value is " + targetValue);
+     
+      if(this.currBTCtix == targetValue ) clearInterval(interval);
+    },50)
+  }
+
+  updateCurrETHtix(rangeETHTixIncrease: number){
+    var increment = this.randomIntRange(0,30);
+    // console.log("increment value " + increment);
+    var targetValue = this.currETHtix + increment;
+
+    if (targetValue >= this.totalETHtix){
+      targetValue=this.totalETHtix;
+    }
+
+    let interval = setInterval(()=>{
+      this.currETHtix ++;
+      this.loadETHProgress= ((this.currETHtix/this.totalETHtix)*100).toFixed(2);
+      console.log("ETH tix target value is " + targetValue);
+      if(this.currETHtix == targetValue ) clearInterval(interval);
+    },100)
+  }
+
+  randomIntRange(min,max)
+  {
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
 }
