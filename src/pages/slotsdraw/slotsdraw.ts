24d06924340bf8883da95ba071fa1ed6ec66f59f
Jasper Lee <jasper.lee.2009@vjc.sg>
2018-08-09 19:46:07 +0800

    slot machine styling

--- src/pages/slotsdraw/slotsdraw.ts
+++ src/pages/slotsdraw/slotsdraw.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as $ from "jquery";
 
+declare var SlotMachine;
+// declare function shuffle();
 /**
  * Generated class for the SlotsdrawPage page.
  *
@@ -14,6 +17,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'slotsdraw.html',
 })
 export class SlotsdrawPage {
+  machine1;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -22,4 +26,95 @@ export class SlotsdrawPage {
     console.log('ionViewDidLoad SlotsdrawPage');
   }
 
+  ngAfterViewInit() {
+    $(document).ready(function () {
+      const btnShuffle = document.querySelector("#casinoShuffle");
+      //first machine
+      const element1 = document.querySelector('#machine1');
+      const machine1 = new SlotMachine(element1, {
+        active: 0,
+        spins: 999999,
+        delay: 500,
+      });
+      machine1.shuffle();
+
+      const element2 = document.querySelector('#machine2');
+      var machine2 = new SlotMachine(element2, {
+        // active: 7,
+        auto: false,
+        direction: 'up'
+      });
+      machine2.shuffle();
+
+      const element3 = document.querySelector('#machine3');
+      var machine3 = new SlotMachine(element3, {
+        // active: 2,
+        auto: false,
+      });
+      machine3.shuffle();
+
+      const element4 = document.querySelector('#machine4');
+      var machine4 = new SlotMachine(element4, {
+        // active: 3,
+        auto: false,
+      });
+      machine4.shuffle();
+
+      const element5 = document.querySelector('#machine5');
+      var machine5 = new SlotMachine(element5, {
+        active: 0,
+        auto: false,
+      });
+      // machine5.shuffle();
+
+      btnShuffle.addEventListener('click', () => {
+        // for (var i=0; i<10; i++){
+        // setTimeout(function(){
+        //   // machine1.shuffle(9999999);
+        //   machine2.shuffle(15);
+        //   machine3.shuffle(20);
+        //   machine4.shuffle(25);
+        //   machine5.shuffle(30);
+        // }, 0);
+      })
+    });
+
+  }
+
+  randomSpin() {
+    $('#casinoShuffle').text('white');
+    const element1 = document.querySelector('#machine1');
+    const machine1 = new SlotMachine(element1, {
+      active: 0,
+      delay: 300,
+    });
+    machine1.shuffle();
+    const element2 = document.querySelector('#machine2');
+    const machine2 = new SlotMachine(element2, {
+      active: 1,
+      delay: 500,
+    });
+    const element3 = document.querySelector('#machine3');
+    const machine3 = new SlotMachine(element3, {
+      active: 1,
+      delay: 800,
+    });
+    const element4 = document.querySelector('#machine4');
+    const machine4 = new SlotMachine(element4, {
+      active: 1,
+      delay: 900,
+    });
+    const element5 = document.querySelector('#machine5');
+    const machine5 = new SlotMachine(element5, {
+      active: 1,
+      delay: 1100,
+    });
+
+    machine1.shuffle();
+    machine2.shuffle();
+    machine3.shuffle();
+    machine4.shuffle();
+    machine5.shuffle();
+    // machine2.shuffle();
+  }
 }
