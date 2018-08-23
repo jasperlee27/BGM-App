943c766825062c2b4ed26a74b276cdff7cd31ce5
Jasper Lee <jasper.lee.2009@vjc.sg>
2018-08-24 00:15:02 +0800

    chat works for separate page

--- src/pages/dummy-chat/dummy-chat.ts
+++ src/pages/dummy-chat/dummy-chat.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as io from 'socket.io-client';
 import { GlobalAuthProvider } from '../../providers/global-auth/global-auth';
 import { Observable } from '../../../node_modules/rxjs/Observable';
-
+// import { Content } from ‘@ionic-angular’;
 /**
  * Generated class for the DummyChatPage page.
  *
@@ -21,20 +21,33 @@ export class DummyChatPage {
   messages: Array<any>;
   username: string;
   message;
+  private toolbarFooterColor: string
+  @ViewChild('content') content: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private auth: GlobalAuthProvider) {
     this.username = this.auth.getUsername();
+    this.toolbarFooterColor = 'dark';
     this.socket = io.connect('http://178.128.50.224:3006');
     console.log("socket for hashing conencted");
 
-    this.getMessages().subscribe(message => {
-      this.messages.push(message);
-      console.log("subscribed and after pushing into msgs  " + this.messages);
-    });
+    // this.getMessages().subscribe(message => {
+    //   // var msgToPush= JSON.parse(message);
+    //   // this.messages.push(message);
+    //   console.log("subscribed and after pushing into msgs  " + this.messages);
+    // });
 
     this.messages = new Array();
   }
 
+  ionViewWillEnter() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      this.content.scrollToBottom();
+    });
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad DummyChatPage');
   }
@@ -42,15 +55,19 @@ export class DummyChatPage {
   ngOnInit() {
     // var username = this.auth.getUsername();
     //CODE FOR SOCKET//
- 
+
     this.socket.on('chat message', (msg: any) => {
-      this.messages.push(msg);
-      console.log(msg);
+      var msgToPush = JSON.parse(msg);
+      this.messages.push(msgToPush);
+      this.scrollToBottom();
+      console.log("Original msg: " + msg);
+      console.log("parsed msg username: " + msgToPush.username);
+      console.log("parsed msg msg: " + msgToPush.msg);
       console.log(this.messages);
     });
 
     //emit to server
-    var objToSend = {username: this.username, msg: " test json msg message Input" };
+    var objToSend = { username: this.username, msg: "test json msg message Input" };
     var jsonToSend = JSON.stringify(objToSend);
     this.socket.emit('chat message', jsonToSend);
 
@@ -62,30 +79,35 @@ export class DummyChatPage {
       console.log("Received chat message here " + data);
     });
 
+    setTimeout(() => {
+      this.content.scrollToBottom(300);//300ms animation speed
+    });
   }
 
   sendMessage() {
-    this.socket.emit('add-message', { text: this.message });
+    var objToSend = { username: this.username, msg: this.message };
+    var jsonToSend = JSON.stringify(objToSend);
+    this.socket.emit('chat message', jsonToSend);
     this.message = '';
   }
 
-  getMessages() {
-    let observable = new Observable(observer => {
-      this.socket.on('chatmessage', (data) => {
-        observer.next(data);
-        console.log("in get messages " + data);
-      });
-    })
-    return observable;
-  }
+  // getMessages() {
+  //   let observable = new Observable(observer => {
+  //     this.socket.on('chatmessage', (data) => {
+  //       observer.next(data);
+  //       console.log("in get messages " + data);
+  //     });
+  //   })
+  //   return observable;
+  // }
 
-  getUsers() {
-    let observable = new Observable(observer => {
-      this.socket.on('users-changed', (data) => {
-        observer.next(data);
-      });
-    });
-    return observable;
-  }
+  // getUsers() {
+  //   let observable = new Observable(observer => {
+  //     this.socket.on('users-changed', (data) => {
+  //       observer.next(data);
+  //     });
+  //   });
+  //   return observable;
+  // }
 
 }
